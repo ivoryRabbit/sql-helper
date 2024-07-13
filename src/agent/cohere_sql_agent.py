9d543f5036af434f760ffa87699640d5adfb5a21
ivoryRabbit <tsi00008@gmail.com>
2024-07-13 17:39:24 +0900

    fix: modify local persistent file path

--- src/agent/cohere_sql_agent.py
+++
@@ -1,9 +0,0 @@
-from src.agent.sql_agent import SQLAgent
-from src.core.chat.cohere import CohereChat
-from src.core.interface.vector_store import VectorStore
-
-
-class CohereSQLAgent(CohereChat, SQLAgent):
-    def __init__(self, vector_store: VectorStore, cohere_api_key=None, cohere_model=None, config=None):
-        SQLAgent.__init__(self, vector_store=vector_store, config=config)
-        CohereChat.__init__(self, api_key=cohere_api_key, model=cohere_model, config=config)
--- src/agent/openai_sql_agent.py
+++
@@ -1,9 +0,0 @@
-from src.agent.sql_agent import SQLAgent
-from src.core.chat.openai import OpenAIChat
-from src.core.interface.vector_store import VectorStore
-
-
-class OpenAISQLAgent(OpenAIChat, SQLAgent):
-    def __init__(self, vector_store: VectorStore, openai_api_key=None, openai_model=None, config=None):
-        SQLAgent.__init__(self, vector_store=vector_store, config=config)
-        OpenAIChat.__init__(self, api_key=openai_api_key, model=openai_model, config=config)
